ab49a30b8ed38626008d13f52ee0a97206263700
Mohamed Mabrouk <mohamed666abdelsalam56@gmail.com>
2024-02-10 14:22:52 +0200

    sfds

--- src/quote.jsx
+++ src/quote.jsx
@@ -65,10 +65,10 @@ function Quote() {
             </p>
             <button className='update-button' onClick={updateIndex} style={{ backgroundColor: COLORS[colorIndex] }}>Next</button>
         </div>
-    </div> : <div className="container">
+    </div> : <section className="container">
         <div className="one"></div>
         <div className="two"></div>
         <div className="three"></div>
-    </div>
+    </section>
 }
 export default Quote
